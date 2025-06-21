c5e6b5efee5187064698945995d8062a2a95c3fb
Soham-shewale195 <shewalesoham195@gmail.com>
2025-06-21 23:19:49 +0530

    array topic

---
+++ four-Arrays.js
@@ -0,0 +1,39 @@
+//============================TOPIC : ARRAYS ===============================================================
+
+// let marks=[56,87,89,76,90];
+// console.log(marks);
+// console.log(marks.length); // provide length of array
+
+// array indices
+// console.log(marks[3]);// we can write specific index also
+// console.log(marks[3]=80);
+
+// Lopping array :
+
+
+// for(let i=0;i<freinds.length;i++)
+// {
+//     console.log(freinds[i]);
+// }
+
+// for-of loop in array
+let freinds=["harshad","vishal","ankit","Atharva","avinash","om","ganesh","kundan"];
+
+for(let fre of freinds)
+{
+    console.log(fre); // we can aslo use for-of to print array .
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
