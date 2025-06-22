1cdf7768dbf70af4a5fc1397aa44e5ca1f3cdca1
Soham-shewale195 <shewalesoham195@gmail.com>
2025-06-22 18:00:44 +0530

    array topic and prac que

--- Third-loops-Strings.js
+++ Third-loops-Strings.js
@@ -82,46 +82,102 @@
 //Q.2 Createa game where you start with any random game number. Ask the user to kepp gusseing th game 
 // number until the user enters correct value.
 
-let gameNum = 7;
+// let gameNum = 7;
 
-let userNum = prompt("Guess a game number :");
-while (userNum != gameNum) 
-{
-    userNum= prompt("You entered wrong number , try again 🤦‍♂️ ")
-}
-console.log("Congratulations !! , you find  right number 🎉🌹.");
-
-
+// let userNum = prompt("Guess a game number :");
+// while (userNum != gameNum) 
+// {
+//     userNum= prompt("You entered wrong number , try again 🤦‍♂️ ")
+// }
+// console.log("Congratulations !! , you find  right number 🎉🌹.");
 
 
+//=============================TOPIC:STRINGS IN JS  ========================================
+
+// let str= "I am iron man";
+// console.log(str);  // to print string 
+// console.log(str.length); // to print string length
+// console.log(str[3]);  // to print specific character in string
+
+// Template literals
+// let specialstring =`Harvey Specter `;  // (`) used in template literals
+// console.log(typeof specialstring);
+
+//e.g= 
+// let obj={
+//     item:"pen",
+//     price:20,
+
+// };
+// console.log("the cost of",obj.item,"is",obj.price,"ruppes");// normal string print 
+// let output= `the cost of ${obj.item} is ${obj.price} ruppes`; // here template literal used .
+// console.log(output);
 
 
+//String Interpolation :
+
+//  let str1 =`two cricket bat price is :${600*2} ruppes `;  // (`) used in template literals
+// console.log(str1);
 
 
+// Escape characters :(\n ,\t)
+// let str="dsadf \n asdfcsd"; // \n is use for next line
+// let str1="dsadf \t asdfcsd"; // used to space between characters
+// console.log(str);
+// console.log(str1);
+
+//========================STRING METHOD/FUNCTIONS=================================================
+//toUpperCase()
+// let str=" dadda tyagi"
+// str=str.toUpperCase(); // (toUpperCase is a method) which makes string Uppercase
+// console.log(str);
+
+//toLowerCase()
+// let str=" DADDA Tyagi"
+// str=str.toLowerCase();// (toLowerCase is a method) which makes string in Lowercase
+// console.log(str);
+
+//trim();
+// let str="       ==  DADDA Tyagi  ==    "  
+// str=str.trim();// trim spaces  from strting and ending.
+// console.log(str);
+
+//slice(str,end?);
+// let str="abcdefg"  
+// str=str.slice(1,5);// start with index 0 ,target specific index
+// console.log(str);
+
+//concat();
+// let str1="SUPER" ;
+// let str2="HERO" ;
+// let result1=str1.concat(str2);// start with index 0 ,target specific index
+// let result2=str2.concat(str1); // we can reverse strings also
+// let result3=str1+str2; // it working same as above , we can add other strins also
+// console.log(result1);
+// console.log(result2);
+// console.log(result3);
 
 
+//replace();
+// let str="The MoMoMoagend";
+// console.log(str.replace("Mo","Le")); // first char is 'search' and 2nd is 'replace'.
+// console.log(str.replaceAll("Mo","Le")); // replace all same values
+
+//charAt();
+// let str="ILoveJava";
+
+// console.log(str.charAt(3)); // we can print specific characters
 
 
+//====================================PRACTICE QUESTIONS ===================================================
+//Q.1 . Prompt the user to enter their full name . Generate a username for them based on the input .
+//start username with @ , followed by their full name and ending with the fullname length.
+//  eg: user name ="sohamshewale" ,username should be="@sohamshewale12"
+ //----->
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+let fullname= prompt("Enter your fullname without spaces :");
+let username="@"+fullname+fullname.length;
+console.log(username);
 
 
 
--- four-Arrays.js
+++ four-Arrays.js
@@ -17,23 +17,117 @@
 // }
 
 // for-of loop in array
-let freinds=["harshad","vishal","ankit","Atharva","avinash","om","ganesh","kundan"];
+// let freinds=["harshad","vishal","ankit","Atharva","avinash","om","ganesh","kundan"];
 
-for(let fre of freinds)
-{
-    console.log(fre); // we can aslo use for-of to print array .
-}
+// for(let fre of freinds)
+// {
+//     console.log(fre); // we can aslo use for-of to print array .
+// }
 
+// let cities=["vinchur","niphad","nashik"];
 
-
-
-
-
-
-
-
+// for(let city of cities)
+// {
+//     console.log(city.toUpperCase());
+// }
 
 
+//======================== PRACTICE QUESTION =======================================
+//  let marks=[85,97,44,37,76,60];
+// let sum=0;
+//  for(i=0;i<6;i++)
+//  {
+//      sum += marks[i];  // we can aslo write it as (sum= sum+marks[i])
+//  }
+//  let avg=sum/marks.length;
+// console.log(`average marks of the class :${avg}`);
+
+// Q2.For given array price of 5 items ->[250,645,300,900,50].All items have an offer of 10% OFF on them .
+//change the array to store the final price after appling offer.
+//----> 
+ 
+// let items=[250,645,300,900,50];
+
+// for(i=0;i<items.length;i++)
+// {
+//     let offer= items[i]/10;
+//     items[i]=items[i]- offer;
+// }
+// console.log(`Value after offer applied :${items} rs`)
+
+//============================== ARRAY METHODS =================================================
+
+// // push() used to add element form end .
+
+// let foodItems=["apple","banana","mango"];
+// console.log(foodItems);
+// foodItems.push("papaya"); //to add single element .
+// foodItems.push("lichi","custard","lemon");//to add multiple element 
+// console.log(foodItems);
 
 
+// pop() used to delete element from end and print it  .
 
+// let foodItems=["apple","banana","mango","lichi","custard"];
+// foodItems.pop(); //to delete single element from end  .
+// let deleteItem=foodItems.pop();// save deleted value it 
+// console.log(deleteItem); 
+
+//toString() convert array into string
+// let foodItems=["apple","banana","mango","lichi","custard"];
+// console.log(foodItems.toString());
+
+//concat() method:
+// let marvel=["ironman","hulk","thor","spiderman","groot"];
+// let dc=["batman","superman","flash","joker"];
+// let indianhero=["shaktiman","krish"];
+// let heroes= marvel.concat(dc,indianhero);
+// console.log(heroes);
+
+// // unshift() method : add element form start.
+// let marvel=["ironman","hulk","thor","spiderman","groot"];
+// marvel.unshift(" antman");
+// console.log(marvel);
+
+// // shift() method : delete element prom start.
+// let marvel=["ironman","hulk","thor","spiderman","groot"];
+// let val=marvel.shift();
+// console.log(val);
+
+// slice() method : returns the piece of array.
+// let marvel=["ironman","hulk","thor","spiderman","groot"];
+// console.log(marvel.slice(1,4));
+
+// splice() method : change the original array (strtidx,deletecount,replace) .
+// let arr=[1,2,3,4,5,6,7,8];
+// arr.splice(2,2,9,10); //here  ,start form indx 2 , delet 2 element , and add 9 and 10 .
+//add element
+// arr.splice(2,0,102)    //here  ,start form indx 2 , delet 0 element , and add 102
+//delete element 
+// arr.splice(3,1,0)    //here  ,start form indx 1 , delet 1 element , and add 0/nothing
+//replace element 
+// arr.splice(3,1,45)    //here  ,start form indx 4 , delet 1 element , and add 45
+// console.log(arr);
+
+
+//============================= PRACTICE QUESTIONS ===================================================
+
+//Q.1 create an array to store companies-->"Bloomberg","Microsoft","Uber","Google","IBM","Netflix"
+//1.Remove the fisrt company fronm the array :
+//--->
+// let companies=["Bloomberg","Microsoft","Uber","Google","IBM","Netflix"];
+// companies.shift();
+// console.log(companies);
+
+//2. Remove Uber & Add Ola in its place
+//-->
+// let companies=["Bloomberg","Microsoft","Uber","Google","IBM","Netflix"];
+// companies.splice(2,1,"ola");
+// console.log(companies);
+
+
+//3.Add Amazon at the end 
+//-->
+let companies=["Bloomberg","Microsoft","Uber","Google","IBM","Netflix"];
+companies.push("Amazon");
+console.log(companies);
