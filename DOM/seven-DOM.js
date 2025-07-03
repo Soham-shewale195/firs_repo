430a3561de029d88aa7db9e80687318e71ebed6f
Soham-shewale195 <shewalesoham195@gmail.com>
2025-07-03 23:20:23 +0530

    Dom topic is remaining

---
+++ DOM/seven-DOM.js
@@ -0,0 +1,31 @@
+// let div1=document.querySelector("div");
+// console.log(div1);
+
+// let id= div1.getAttribute("id"); // find the attribute "id" name
+// console.log(id);    
+
+// let id1= div1.getAttribute("name");  // find the  "name" of attribute
+// console.log(id1);
+
+// let para= document.querySelector("p");
+// console.log(para.getAttribute("class")); // we can find "class" name using  :para.getAttribute("class") this line
+
+// let para= document.querySelector("p");
+// console.log(para.setAttribute("class","abc")); //using "setAttribute" we can change attribute name 
+
+// let div=document.querySelector("div")
+// div.style.backgroundColor="red";         // using "style" we can add css in JS
+// div.style.fontSize="20px";
+// div.innerText="Hello";
+
+//Insert element:
+
+let newbtn=document.createElement("button");
+newbtn.innerText="Click me"
+console.log(newbtn);
+
+
+
+
+
+
